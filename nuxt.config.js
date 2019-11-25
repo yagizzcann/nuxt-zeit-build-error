d6123df773e381bf223f9704246b6bb1e4b3a32c
yağız can <yycc_95@hotmail.com>
2019-11-25 13:49:35 +0300

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,76 @@
+
+import settings from './settings'
+
+module.exports = {
+ // srcDir: __dirname,
+ // buildDir: '_nuxt/app',
+  //lambdaName: 'index',
+  env: {
+    baseUrl: process.env.BASE_URL || `${settings.baseAPIUrl}`,
+  },
+  loading: {
+    color: '#e04950',
+    height: '2px'
+  },
+  modules: [
+    //'@nuxtjs/pwa',
+    '@nuxtjs/axios',
+    ['nuxt-i18n', {
+      baseUrl: 'https://www.example.com',
+      seo: false,
+      lazy: true,
+      parsePages: false,
+      detectBrowserLanguage: false,
+      routesNameSeparator: '/',
+      locales: [{
+          code: 'tr',
+          iso: 'tr',
+          name: 'Turkish',
+          file: 'tr.json',
+        },
+        {
+          code: 'en',
+          iso: 'en',
+          name: 'English',
+          file: 'en.json',
+        },
+
+      ],
+      langDir: 'locales/',
+      defaultLocale: 'tr',
+      strategy: 'prefix_except_default',
+      vueI18n: {
+        fallbackLocale: 'tr',
+        messages: {
+          tr: require('./locales/tr.json'),
+          en: require('./locales/en.json')
+        }
+      }
+    }],
+  ],
+   plugins: [
+    '~/plugins/filters'
+  ],
+  axios: {
+    baseURL: 'https://api.hackerwebapp.com'
+  },
+  render: {
+     http2: {
+       push: true
+     },
+    static: {
+      maxAge: '1y',
+      setHeaders(res, path) {
+        if (path.includes('sw.js')) {
+          res.setHeader('Cache-Control', `public, max-age=${15 * 60}`)
+        }
+      }
+    }
+  },
+  build:{
+    //publicPath: '_nuxt/app',
+  },
+  router: {
+  },
+
+}
---
+++ settings.js
@@ -0,0 +1,30 @@
+const setting = {
+  title: 'Example',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the settings right-panel
+   */
+  showSettings: true,
+
+
+   /**
+   * @type {string} 
+   * @description api base URI
+   */
+  baseAPIUrl: 'https://example.com/',
+  /**
+   * @type {string | array} 'production' | ['production', 'development']
+   * @description Need show err logs component.
+   * The default is only used in the production env
+   * If you want to also use it in dev, you can pass ['production', 'development']
+   */
+  errorLog: ['production', 'development']
+
+
+
+
+}
+
+
+export default setting;
